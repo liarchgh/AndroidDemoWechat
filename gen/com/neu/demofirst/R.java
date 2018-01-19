33fccd80d622126e43f702a80b53589ccd82a652
leesue <liarchgh@users.noreply.github.com>
2018-01-19 11:43:54 +0800

    use dynamic fragment

--- gen/com/neu/demofirst/R.java
+++ gen/com/neu/demofirst/R.java
@@ -29,25 +29,26 @@ public final class R {
         public static final int wx_talklist=0x7f02000f;
     }
     public static final class id {
+        public static final int bt0=0x7f05000b;
         public static final int fl0=0x7f050006;
         public static final int lastMessage=0x7f050009;
-        public static final int ll0=0x7f05000e;
+        public static final int ll0=0x7f05000c;
         public static final int myIcon=0x7f050004;
         public static final int num=0x7f050007;
         public static final int otherIcon=0x7f050000;
-        public static final int pageSlide=0x7f05000b;
-        public static final int passWord=0x7f050012;
-        public static final int passWordLl=0x7f050011;
+        public static final int passWord=0x7f050013;
+        public static final int passWordLl=0x7f050012;
         public static final int rl0=0x7f050005;
         public static final int sentence=0x7f050002;
-        public static final int sentencesLl=0x7f05000c;
-        public static final int talksLV=0x7f05000f;
+        public static final int sentencesLl=0x7f05000e;
+        public static final int talksLV=0x7f050010;
+        public static final int testFragment=0x7f05000d;
         public static final int time=0x7f05000a;
         public static final int toMe=0x7f050003;
         public static final int toOther=0x7f050001;
-        public static final int toSend=0x7f05000d;
+        public static final int toSend=0x7f05000f;
         public static final int userName=0x7f050008;
-        public static final int userNameLl=0x7f050010;
+        public static final int userNameLl=0x7f050011;
     }
     public static final class layout {
         public static final int activity_one_sentence=0x7f030000;
--- src/com/neu/demofirst/Slide.java
+++ src/com/neu/demofirst/Slide.java
@@ -7,6 +7,10 @@ import java.util.zip.Inflater;
 import com.neu.demoUtil.PageSlide;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.text.Layout;
@@ -22,13 +26,23 @@ public class Slide extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_slide);
 		
-		vp = (ViewPager)Slide.this.findViewById(R.id.pageSlide);
-		List<View>vs = new ArrayList<View>();
-		LayoutInflater ift = Slide.this.getLayoutInflater();
-		vs.add(ift.inflate(R.layout.activity_one_sentence, null));
-		vs.add(ift.inflate(R.layout.login, null));
-		vs.add(ift.inflate(R.layout.activity_talk, null));
-		ps = new PageSlide(vs);
-		vp.setAdapter(ps);
+//		vp = (ViewPager)Slide.this.findViewById(R.id.pageSlide);
+//		List<View>vs = new ArrayList<View>();
+//		LayoutInflater ift = Slide.this.getLayoutInflater();
+//		vs.add(ift.inflate(R.layout.activity_one_sentence, null, false));
+//		vs.add(ift.inflate(R.layout.login, null));
+//		vs.add(ift.inflate(R.layout.activity_talk, null));
+//		ps = new PageSlide(vs);
+//		vp.setAdapter(ps);
+	}
+	public void useFragment(View v) {
+		Dialog dl = new Dialog(Slide.this);
+		dl.setTitle("Begin");
+		dl.show();
+		
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction tt = fm.beginTransaction();
+		tt.replace(R.id.testFragment, new ReplaceFragment());
+		tt.commit();
 	}
 }
